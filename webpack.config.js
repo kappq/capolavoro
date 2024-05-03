218af5f0cbda23d55c99fce3ab4fa4120bc0193c
Kappa <f.cappetti.05@gmail.com>
2024-05-03 23:52:38 +0200

    Basic webpack and d3 setup

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Capolavoro',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: {
+          loader: 'csv-loader',
+          options: {
+            header: true,
+            skipEmptyLines: true,
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
